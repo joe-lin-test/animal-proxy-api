19ceb204b2e5fadf57064e1b34146fe51ad9e314
jiahongL <>
2018-09-11 14:54:42 +0800

    upload php.html

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+$url_params = '';
+foreach ($_GET as $name => $value) {
+  $url_params = $url_params . '&' . $name . '=' . $value; 
+}
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL" . $url_params,
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 60,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_NONE,
+  CURLOPT_CUSTOMREQUEST => "GET",
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
