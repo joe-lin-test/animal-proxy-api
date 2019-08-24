5238e918c62f7619e6d45454cc543959520bf1b2
Joe Lin <>
2019-08-24 18:39:26 +0800

    [調整] 增加路由

---
+++ davai.php
@@ -0,0 +1,377 @@
+<?php
+class Davai
+{
+    /**
+     * Stores the data of the routes so we can generate a reversed route from here.
+     *
+     * @var array
+     */
+    public $routes = [];
+    /**
+     * The current URL.
+     *
+     * @var string
+     */
+    public $url = '';
+    /**
+     * The current method.
+     *
+     * @var string
+     */
+    public $method = '';
+    /**
+     * The rules of the capture groups.
+     *
+     * @var array
+     */
+    public $rules = ['*' => '.+?',
+                     'i' => '[0-9]++',
+                     'a' => '[0-9A-Za-z]++',
+                     'h' => '[0-9A-Fa-f]++'];
+    /**
+     * Stores the variables for passing though the functions.
+     *
+     * @var array
+     */
+    private $variables = [];
+    /**
+     * Convert the url string to the array, split by the slash.
+     *
+     * @var array
+     */
+    private $parsedUrl   = [];
+    /**
+     * Same as the $parsedUrl, but the path instead of the current url.
+     *
+     * @var array
+     */
+    private $parsedPath  = [];
+    /**
+     * We combined the parsed path and the parsed url together, and stores them to here, also added some extra informations.
+     *
+     * @var array
+     */
+    private $parsedGroup = [];
+    private $records = [];
+    private $basePath = '';
+    /**
+     * CONSTRUCT
+     *
+     * @codeCoverageIgnore
+     */
+    function __construct()
+    {
+        /** Set the current url */
+        $this->url = isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : NULL;
+        /** And the method */
+        $this->method = isset($_SERVER['REQUEST_METHOD']) ? strtoupper($_SERVER['REQUEST_METHOD']) : '';
+    }
+    /**
+     * CALL
+     *
+     * @param string $name   The function name.
+     * @param array  $args   The arguments.
+     */
+    function __call($name, $args)
+    {
+        switch($name)
+        {
+            case 'get'   :
+            case 'post'  :
+            case 'put'   :
+            case 'delete':
+            case 'patch' :
+                $path      = $args[0];
+                $func      = $args[1];
+                $routeName = isset($args[2]) ? $args[2] : null;
+                return $this->map(strtoupper($name), $path, $func, $routeName);
+                break;
+            case 'recordGet'   :
+            case 'recordPost'  :
+            case 'recordPut'   :
+            case 'recordDelete':
+            case 'recordPatch' :
+                $recordName = $args[0];
+                $path       = $this->records[$recordName];
+                $func       = $args[1];
+                $name       = substr($name, 6);
+                $this->map(strtoupper($name), $path, $func);
+                return $this;
+                break;
+        }
+    }
+    /**
+     * Set Base Path
+     *
+     *
+     *
+     * @param
+     *
+     * @return Davai
+     */
+    function setBasePath($path)
+    {
+        $this->basePath = $path;
+        return $this;
+    }
+    /**
+     * Record
+     *
+     */
+    function record($records)
+    {
+        $this->records = $records;
+        foreach($records as $name => $path)
+        {
+            /** Separate the path by the slash */
+            $path        = explode('/', $path);
+            $parsedPath  = array_filter(array_map('trim', $path));
+            /** Separate the current url by the slash */
+            $url         = explode('/', strtok($this->url, '?'));
+            $parsedUrl   = array_filter(array_map('trim', $url));
+            $parsedGroup = $this->groupUrl($parsedPath, $parsedUrl);
+            $this->storeRoute($name, $parsedGroup);
+        }
+        return $this;
+    }
+    /**
+     * The url mapping function, here to capture the urls.
+     *
+     * @param string      $method   The method, ex: GET, POST, DELETE.
+     * @param string      $path     The path of the route.
+     * @param mixed       $func     The callback function, can be a anonymous function or a function name, or even 'class@function'.
+     * @param string|null $name     The name of the route for reverse routing.
+     *
+     * @return Davai
+     */
+    function map($method, $path, $func, $name = null)
+    {
+        /** Clean the last parsed group */
+        $this->parsedGroup = [];
+        /** Path with the base path */
+        $path = $this->basePath . $path;
+        /** Separate the path by the slash */
+        $path             = explode('/', $path);
+        $this->parsedPath = array_filter(array_map('trim', $path));
+        /** Separate the current url by the slash */
+        $url              = explode('/', strtok($this->url, '?'));
+        $this->parsedUrl  = array_filter(array_map('trim', $url));
+        /** Group the path and the url together */
+        $this->groupUrl();
+        /** Save the route to the routes array for reverse routing when the name is not a null */
+        if($name !== null)
+            $this->storeRoute($name, $this->parsedGroup);
+        /** Validate the rules with the current url */
+        if(!$this->validateRules())
+            return $this;
+        /** Convert the captured url contents to the variables */
+        $this->analyzeVariables();
+        /** The method must be right, or GGWP */
+        if($this->method !== strtoupper($method))
+            return $this;
+        /** Parse the function name when it's a string not a REAL function */
+        if(is_string($func) && strpos($func, '#') !== false)
+        {
+            /** we split the string by the hashtag, get the class name and the function name */
+            $funcGroup = explode('#', $func);
+            $className = $funcGroup[0];
+            $funcName  = $funcGroup[1];
+            /** Call the callback which inside of the class */
+            call_user_func_array([$className, $funcName], $this->variables);
+        }
+        else
+        {
+            /** Or just call the callback directly */
+            call_user_func_array($func, $this->variables);
+        }
+        return $this;
+    }
+    /**
+     * Generate the reversed route path.
+     *
+     * @param string $name        The name of the route.
+     * @param array  $variables   The variables, key as the variable name.
+     *
+     * @return string|bool
+     */
+    function generate($name, $variables = null)
+    {
+        if(!isset($this->routes[$name]))
+            return false;
+        $link = '';
+        /** Analyze the paths, and apply the variables, convert them back into a string */
+        foreach($this->routes[$name]['paths'] as $singlePartial)
+        {
+            $variableName = $singlePartial['variable'];
+            /** Just use the variable name as the path when it's a "pure" variable */
+            if($singlePartial['isPure'])
+                $link .= $variableName . '/';
+            else
+                /** Or get the value for the variable and replace it */
+                if(isset($variables[$variableName]))
+                    $link .= $variables[$variableName] . '/';
+                /** Stop parsing the path if there's no matched value for this variable */
+                else
+                    break;
+        }
+        return $this->basePath . $link;
+    }
+    /**
+     * Parse the group and stores them to the routes array so we can reverse routing with it.
+     *
+     * @param string $name    The name of the route.
+     * @param array  $group   The parsed group.
+     *
+     * @return Davai
+     */
+    function storeRoute($name, $group)
+    {
+        $paths = [];
+        foreach($group as $single)
+        {
+            $variable = $single['isPure'] ? $single['rule'] : $single['variable'];
+            $paths[] = ['variable' => $variable,
+                        'isPure'   => $single['isPure']];
+        }
+        $this->routes[$name] = ['paths' => $paths];
+        return $this;
+    }
+    /**
+     * Add a custom rule.
+     *
+     * @param string $name    The shorthand of the rule.
+     * @param string $regEx   The regEx.
+     *
+     * @return Davai
+     */
+    function addRule($name, $regEx)
+    {
+        $this->rules[$name] = $regEx;
+        return $this;
+    }
+    /**
+     * Make sure the current url is valid with the rules.
+     *
+     * @return bool
+     */
+    function validateRules()
+    {
+        $index     = -1;
+        $length    = count($this->parsedGroup);
+        $urlLength = count($this->parsedUrl);
+        foreach($this->parsedGroup as $singleGroup)
+        {
+            $index++;
+            extract($singleGroup);
+            /** When this is the last partial */
+            if($index == $length - 1)
+                /** Return false if it's not a lazy partial, and there's no more partials in the url, */
+                /** and this last partial is not a pure partial */
+                /** ex: "/public/[i:userId]" but "/public/" in the url */
+                if(!$isPure && !$isLazy && !isset($this->parsedUrl[$index + 1]))
+                    return false;
+            /** Return false if it's a pure rule but not the same as the captured content */
+            if($isPure && $rule != $content)
+                return false;
+            /** Skip when it's a pure rule and the same as the captured content, */
+            if($isPure && $rule == $content)
+                /** But return false if this partial is the last one, and there's more partials in the url */
+                /** ex: "/hello/" but "/hello/world/" in the url */
+                if($index == $length - 1 && $index < $urlLength - 1)
+                    return false;
+                /** Going to the next partial if there's more partials in the url */
+                elseif($index != $urlLength - 1)
+                    continue;
+            /** Skip it when it's a lazy rule, */
+            if($isLazy)
+                /** and we captured the empty content, */
+                if(!$content)
+                    return true;
+            /** Use regEx to validate the captured content */
+            $regEx = $this->getRule($rule);
+            preg_match($regEx, $content, $matched);
+            /** Return false if the content is not matched with the regEx */
+            if(!isset($matched[0]) || $content != $matched[0])
+                return false;
+        }
+        return true;
+    }
+    /**
+     * Convert a string to a regEx or get the regEx from the rule list.
+     *
+     * @param  string $ruleName   The name of the rule.
+     *
+     * @return string
+     */
+    function getRule($ruleName)
+    {
+        $isSplit = strpos($ruleName, '|') !== false;
+        if($isSplit)
+            return '/^(' . $ruleName . ')$/';
+        elseif(isset($this->rules[$ruleName]))
+            return '/^(' . $this->rules[$ruleName] . ')$/';
+        else
+            return '/^(' . $ruleName . ')$/';
+    }
+    /**
+     * Make the captured url contents as a pair with the rules.
+     *
+     * @return Davai
+     */
+    function analyzeVariables()
+    {
+        $this->variables = [];
+        foreach($this->parsedGroup as $singleGroup)
+        {
+            $variableName = $singleGroup['variable'];
+            $content      = $singleGroup['content'];
+            if($variableName)
+                array_push($this->variables, $content);
+        }
+        return $this;
+    }
+    /**
+     * Separate the rule tags, and match it with the url content, then stores to a the parsed group array.
+     *
+     * @return Davai
+     */
+    function groupUrl($parsedPath = null, $parsedUrl = null)
+    {
+        $isAlone     = $parsedPath != null;
+        $parsedPath  = $parsedPath ?: $this->parsedPath;
+        $parsedUrl   = $parsedUrl  ?: $this->parsedUrl;
+        $parsedGroup = [];
+        foreach($parsedPath as $index => $singlePath)
+        {
+            $matchedUrl = isset($parsedUrl[$index]) ? $parsedUrl[$index] : false;
+            $partial    = $this->separatePartial($singlePath, $matchedUrl);
+            if($isAlone)
+                $parsedGroup[] = $partial;
+            else
+                $this->parsedGroup[] = $partial;
+        }
+        return $parsedGroup;
+    }
+    /**
+     * Separate the rule tag, and parse it, also match it with the same level url content then return the whole informations.
+     *
+     * @param string $partial          The rule tag.
+     * @param string $matchedContent   The matched url content.
+     *
+     * @return array
+     */
+    function separatePartial($partial, $matchedContent)
+    {
+        $isLazy      = substr($partial, -2, 1) === '?';
+        $isTag       = substr($partial,  0, 1) === '[';
+        $parsedPartial = explode(':', $partial);
+        $rule          = ltrim($parsedPartial[0], '[');
+        $variable      = isset($parsedPartial[1]) ? rtrim($parsedPartial[1], $isLazy ? '?]' : ']') : '';
+        return ['rule'     => $rule,
+                'variable' => $variable !== '' ? $variable : false,
+                'isLazy'   => $isLazy,
+                'isPure'   => !$isTag,
+                'content'  => $matchedContent];
+    }
+}
+?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once "router.php";
+require_once "davai.php";
+
+$davai = new Davai();
 
 $origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';  
 
@@ -12,37 +14,270 @@ if(in_array($origin, $allow_origin)){
     header('Access-Control-Allow-Origin:'.$origin);       
 }
 
-$url_params = '';
-foreach ($_GET as $name => $value) {
-  $url_params = $url_params . '&' . $name . '=' . $value; 
-}
+// 舊api
+$davai->get('/index.php', function(){
 
-$curl = curl_init();
+  $url_params = '';
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "http://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL" . $url_params,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 60,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_NONE,
-  CURLOPT_CUSTOMREQUEST => "GET",
-));
+  foreach ($_GET as $name => $value) {
+    $url_params = $url_params . '&' . $name . '=' . $value; 
+  }
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+  $curl = curl_init();
 
-curl_close($curl);
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL" . $url_params,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 60,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_NONE,
+    CURLOPT_CUSTOMREQUEST => "GET",
+  ));
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
 
-route('/data', function () {
-  return ;
+  curl_close($curl);
+
+  if ($err) {
+    echo "cURL Error #:" . $err;
+  } else {
+    echo $response;
+  }
+
 });
 
-$action = $_SERVER['REQUEST_URI'];
-dispatch($action);
+// 獲取寵物資料列表
+$davai->get('/animals', function(){
+
+  $url_params = '';
+
+  foreach ($_GET as $name => $value) {
+    $url_params = $url_params . '&' . $name . '=' . $value; 
+  }
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://data.coa.gov.tw/Service/OpenData/TransService.aspx?UnitId=QcbUEzN6E6DL" . $url_params,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 60,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_NONE,
+    CURLOPT_CUSTOMREQUEST => "GET"
+  ));
+
+  $response = curl_exec($curl);
+  $err = curl_error($curl);
+
+  curl_close($curl);
+
+  $res = '';
+
+  if ($err) {
+
+    $res = array(
+      "success" => false,
+      "result" => null,
+      "errorMessage" => "server error",
+    );
+
+    echo json_encode( $res );
+
+  } else {
+
+    $res = array(
+      "success" => true,
+      "result" => json_decode($response),
+      "errorMessage" => null,
+    );
+
+    echo json_encode( $res );
+
+  }
+
+});
+
+// 01.獲取狀態代碼列表
+$davai->get('/status', function(){
+
+  $json = file_get_contents("./json/status.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 02.獲取動物性別代碼列表
+$davai->get('/sex', function(){
+
+  $json = file_get_contents("./json/sex.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+
+// 03.動物體型代碼表
+$davai->get('/body-type', function(){
+
+  $json = file_get_contents("./json/body-type.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 04.年紀代碼表
+$davai->get('/age', function(){
+
+  $json = file_get_contents("./json/age.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 05.縣市代碼表
+$davai->get('/area', function(){
+
+  $json = file_get_contents("./json/area.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 06.是否絕育代碼表
+$davai->get('/sterilization', function(){
+
+  $json = file_get_contents("./json/sterilization.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 07.是否施打狂犬病代碼表
+$davai->get('/bacterin', function(){
+
+  $json = file_get_contents("./json/bacterin.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 08.類型
+$davai->get('/kind', function(){
+
+  $json = file_get_contents("./json/kind.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 09.毛色
+$davai->get('/colour', function(){
+
+  $json = file_get_contents("./json/colour.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+
+// 10.收容中心
+$davai->get('/shelter', function(){
+
+  $json = file_get_contents("./json/shelter.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 11.回饋類型
+$davai->get('/feedback-type', function(){
+
+  $json = file_get_contents("./json/feedback-type.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
+
+// 12.議題狀態
+$davai->get('/issues-status', function(){
+
+  $json = file_get_contents("./json/issues-status.json");
+
+  $res = array(
+    "success" => true,
+    "result" => json_decode($json),
+    "errorMessage" => null,
+  );
+
+  echo json_encode($res);
+
+});
